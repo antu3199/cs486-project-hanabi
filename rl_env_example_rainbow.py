64554ab2dc305507d1666546d2713012278cb89c
Anthony Tu <antu3199@gmail.com>
2019-07-11 16:55:04 -0400

    Auto Push

--- rl_env_example_rainbow.py
+++ rl_env_example_rainbow.py
@@ -64,7 +64,7 @@ class Runner(object):
     return rewards
 
 if __name__ == "__main__":
-  flags = {'players': 2, 'num_episodes': 1, 'agent_class': 'SimpleAgent'}
+  flags = {'players': 2, 'num_episodes': 1, 'agent_class': 'SimpleAgent', 'rainbow': 0}
   options, arguments = getopt.getopt(sys.argv[1:], '',
                                      ['players=',
                                       'num_episodes=',
